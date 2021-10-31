52499b1bba4a5a18de73e7c9a34eb8c8a2a344e8
Nure-Alam-sidddique <74979607+Nure-Alam-sidddique@users.noreply.github.com>
2021-10-31 20:52:56 +0600

    Read, Delete and create Api Complete

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-//  server require
 const { MongoClient } = require("mongodb");
-require('dotenv').config();
+const cors = require('cors');
+require("dotenv").config();
+const ObjectId = require('mongodb').ObjectId;
+
+// Middlewire
+app.use(cors());
+app.use(express.json());
+//  Network URI
 const uri =
-    `mongodb+srv://${process.env.DB_USER}: ${process.env.DB_PASS}@cluster0.zrqkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// console.log(uri);
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrqkd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -14,6 +19,31 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        await client.connect();
+        const database = client.db('PlaceDestination');
+        const placeCollection = database.collection('Places');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = placeCollection.find({});
+            const result = await cursor.toArray();
+            res.json(result);
+        })
+
+        //  Post API
+        app.post('/services', async (req, res) => {
+            const placeInfo = req.body;
+            const result = await placeCollection.insertOne(placeInfo);
+            res.json(result);
+        })
+
+        // Delete API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await placeCollection.deleteOne(query);
+            res.json(result);
+        })
         
     }
     finally {
@@ -23,13 +53,26 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get('/hello', (req, res) => {
-    res.send("Hello Naznin , How are You!!")
-})
 app.get('/', (req, res) => {
-    res.send("Hello Nurnaz Travelo DB");
+    res.send('Hello NurNaz Travelo');
+})
+app.get('/hello', (req, res) => {
+    res.send("Welcome to NurNaz Travelo");
 })
 
 app.listen(port, () => {
     console.log(`NurNaz Travelo Server Running Port ${port}`);
 })
+
+/**
+ * server side Command :
+ * git add . , git commit, git push
+ * git push heroku main
+ *
+ *
+ * Client side :
+ * git add . , git commit , git push
+ * git run build
+ *
+ * firebase deploy
+ */
