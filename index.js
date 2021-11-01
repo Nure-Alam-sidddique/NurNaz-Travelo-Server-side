3093716c099d0ad6b942d010eccc4c1f608b0fa8
Nure-Alam-sidddique <74979607+Nure-Alam-sidddique@users.noreply.github.com>
2021-11-01 21:14:48 +0600

    Again Get file recheck

--- index.js
+++ index.js
@@ -25,15 +25,6 @@ async function run() {
         const placeCollection = database.collection('Places');
         const bookingCollection = database.collection('orders');
 
-        if (process.env.NODE_ENV === "production") {
-          app.use(express.static(path.join(__dirname, "front_end", "build")));
-
-          app.get("*", (req, res) => {
-            res.sendFile(
-              path.join(__dirname, "front_end", "build", "index.html")
-            );
-          });
-        }
         // GET API
         app.get('/services', async (req, res) => {
             const cursor = placeCollection.find({});
